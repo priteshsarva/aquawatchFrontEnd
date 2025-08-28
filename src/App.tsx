d311419df28cbed5950ff0bb397e5e068cee8623
priteshsarva <priteshsarva9825@gmail.com>
2025-08-29 00:30:51 +0530

    updaed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route ,Navigate} from 'react-router-dom';
 
 import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
